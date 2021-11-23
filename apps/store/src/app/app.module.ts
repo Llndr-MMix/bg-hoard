8295203b36a01c5ccbfde60bef98d7d9ec701b9d
Lllndr-MMix <llndr.2k9@gmail.com>
2021-11-23 12:38:07 +0100

    lab2

--- apps/store/src/app/app.module.ts
+++ apps/store/src/app/app.module.ts
@@ -3,11 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-
+import{ MatCardModule} from '@angular/material/card'
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    MatCardModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
