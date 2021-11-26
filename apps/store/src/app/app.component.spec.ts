d524af8417b2a89a8676833776b971d303ee365c
Lllndr-MMix <llndr.2k9@gmail.com>
2021-11-26 11:29:55 +0100

    new

--- apps/store/src/app/app.component.spec.ts
+++ apps/store/src/app/app.component.spec.ts
@@ -1,11 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreUiSharedModule } from '@bg-hoard/store/ui-shared';
+import { title } from 'process';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [HttpClientModule,RouterTestingModule,StoreUiSharedModule],
       declarations: [AppComponent],
     }).compileComponents();
   });
@@ -16,18 +19,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'store'`, () => {
+  it(`should have as title 'Board Game Hoard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('store');
+    expect(app.title).toEqual('Board Game Hoard');
   });
 
-  it('should render title', () => {
+  it('should render header', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome to store!'
-    );
+    expect(compiled.querySelector('bg-hoard-header')).toBeTruthy();
   });
 });
